86a9821ed131575c506d274f74451828b38e80bb
Kirti Harode <kikihrd20@gmail.com>
2022-01-26 22:10:58 -0800

    styling dropdown

--- frontend/components/headers/splash_header.jsx
+++ frontend/components/headers/splash_header.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Link, withRouter} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { logout } from '../../actions/session_actions';
 import smallLogo from '../../../app/assets/images/in_alpha.png'
-import Home from '../../../app/assets/images/home_icon.png'
 import { FaHome, FaUserFriends, FaUserGraduate, FaSearch, FaBell} from "react-icons/fa";
 import {IoIosBriefcase } from "react-icons/io";
 import { IoChatboxEllipses } from "react-icons/io5";
+
 class LoggedIn extends React.Component {
     constructor(props){
         super(props)
+        this.state = {hidden: true}
+        this.hiddenFalse = this.hiddenFalse.bind(this)
+        this.hiddenTrue = this.hiddenTrue.bind(this)
+    }
+
+    hiddenFalse(){
+        this.setState({hidden: false})
+    }
+
+    hiddenTrue(){
+        this.setState({hidden: true})
     }
 
     render(){
@@ -66,8 +78,34 @@ class LoggedIn extends React.Component {
                     </Link>
 
                     <div className='me-dropdown'>
-                        <FaUserGraduate className='profile-icon'/>
-                        <h2 className='me-heading'>Me</h2>
+                        <button className='dropdown-button' onFocus={this.hiddenFalse} onBlur={this.hiddenTrue}>
+                            <div className='profile-icon-div'>
+                                <FaUserGraduate className='profile-icon'/>
+                                <h2 className='me-heading'>Me</h2>
+                            </div>
+                            <ul className={this.state.hidden ? 'hidden-dropdown' : 'reveal-dropdown'} onClick={e => e.stopPropagation()} >
+                                <div className='dropdown-top'>
+                                    <div className='user-profile-div'>
+                                        <p>User Profile pic goes here</p>
+                                        <div className='user-details'>
+                                            <h2>{this.props.currentUser.fname} {this.props.currentUser.lname}</h2>
+                                            <h3>User bio</h3>
+                                        </div>
+                                    </div>
+                                    <div className='view-profile-button' >
+                                    
+                                        <p onClick={
+                                            ()=>{this.props.history.push(`/users/${this.props.currentUser.id}`); 
+                                            this.hiddenTrue()
+                                        }} 
+                                        className='view-profile'>View Profile</p>
+                                    </div>
+                                </div>
+                                <div className='logout-button' onClick={this.props.logout}>
+                                    <p>Logout</p>
+                                </div>
+                            </ul>
+                        </button>
                     </div>
 
                 
